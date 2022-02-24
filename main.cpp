929083dbfc1700505576b60210f05357fa81eb32
SultanMad <saipov.sultan46274@mail.ru>
2022-02-25 00:13:45 +0400

    Задача на программирование: вторичный индекс в базе данных.

--- main.cpp
+++ main.cpp
@@ -1,141 +1,58 @@
 #include "test_runner.h"
-#include <limits>
-#include <random>
-#include <unordered_set>
+#include "database.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-struct Address {
-    string city, street;
-    int building;
+void TestRangeBoundaries() {
+    const int good_karma = 1000;
+    const int bad_karma = -10;
 
-    bool operator==(const Address& other) const {
-        return city == other.city && street == other.street && building == other.building;
-    }
-};
+    Database db;
+    db.Put({"id1", "Hello there", "master", 1536107260, good_karma});
+    db.Put({"id2", "O>>-<", "general2", 1536107260, bad_karma});
 
-struct Person {
-    string name;
-    int height;
-    double weight;
-    Address address;
+    int count = 0;
+    db.RangeByKarma(bad_karma, good_karma, [&count](const Record&) {
+        ++count;
+        return true;
+    });
 
-    bool operator==(const Person& other) const {
-        return name == other.name && height == other.height
-               && weight == other.weight && address == other.address;
-    }
-};
-
-struct AddressHasher {
-    size_t operator() (const Address& address) const {
-        const size_t prime = 3301;
-        hash<string> str_hash;
-        hash<int> int_hash;
-        return  (str_hash(address.city)*prime*prime) +
-                (str_hash(address.street)*prime) + int_hash(address.building);
-    }
-};
-
-struct PersonHasher {
-    size_t operator() (const Person& person) const {
-        const size_t prime = 3301;
-        hash<string> str_hash;
-        hash<int> int_hash;
-        hash<double> double_hash;
-        AddressHasher address_hasher;
-        return  (str_hash(person.name)*prime*prime*prime) +
-                (int_hash(person.height)*prime*prime) + (double_hash(person.weight)*prime) +
-                address_hasher(person.address);
-    }
-};
-
- const vector<string> WORDS = {
-        "Kieran", "Jong", "Jisheng", "Vickie", "Adam", "Simon", "Lance",
-        "Everett", "Bryan", "Timothy", "Daren", "Emmett", "Edwin", "List",
-        "Sharon", "Trying", "Dan", "Saad", "Kamiya", "Nikolai", "Del",
-        "Casper", "Arthur", "Mac", "Rajesh", "Belinda", "Robin", "Lenora",
-        "Carisa", "Penny", "Sabrina", "Ofer", "Suzanne", "Pria", "Magnus",
-        "Ralph", "Cathrin", "Phill", "Alex", "Reinhard", "Marsh", "Tandy",
-        "Mongo", "Matthieu", "Sundaresan", "Piotr", "Ramneek", "Lynne", "Erwin",
-        "Edgar", "Srikanth", "Kimberly", "Jingbai", "Lui", "Jussi", "Wilmer",
-        "Stuart", "Grant", "Hotta", "Stan", "Samir", "Ramadoss", "Narendra",
-        "Gill", "Jeff", "Raul", "Ken", "Rahul", "Max", "Agatha",
-        "Elizabeth", "Tai", "Ellen", "Matt", "Ian", "Toerless", "Naomi",
-        "Rodent", "Terrance", "Ethan", "Florian", "Rik", "Stanislaw", "Mott",
-        "Charlie", "Marguerite", "Hitoshi", "Panacea", "Dieter", "Randell", "Earle",
-        "Rajiv", "Ted", "Mann", "Bobbie", "Pat", "Olivier", "Harmon",
-        "Raman", "Justin"
-};
-
-void TestSmoke() {
-    vector<Person> points = {
-            {"John", 180, 82.5, {"London", "Baker St", 221}},
-            {"Sherlock", 190, 75.3, {"London", "Baker St", 221}},
-    };
-
-    unordered_set<Person, PersonHasher> point_set;
-    for (const auto& point : points) {
-        point_set.insert(point);
-    }
-
-    ASSERT_EQUAL(points.size(), point_set.size());
-    for (const auto& point : points) {
-        ASSERT_EQUAL(point_set.count(point), static_cast<size_t>(1));
-    }
+    ASSERT_EQUAL(2, count);
 }
 
-void TestPurity() {
-    Person person = {"John", 180, 82.5, {"London", "Baker St", 221}};
-    PersonHasher hasher;
+void TestSameUser() {
+    Database db;
+    db.Put({"id1", "Don't sell", "master", 1536107260, 1000});
+    db.Put({"id2", "Rethink life", "master", 1536107260, 2000});
+    int count = 0;
+    db.AllByUser("master", [&count](const Record&) {
+        ++count;
+        return true;
+    });
+    db.Erase("id2");
 
-    auto hash = hasher(person);
-    for (size_t t = 0; t < 100; ++t) {
-        ASSERT_EQUAL(hasher(person), hash);
-    }
+    ASSERT_EQUAL(2, count);
 }
 
-void TestDistribution() {
-    auto seed = 42;
-    mt19937 gen(seed);
+void TestReplacement() {
+    const string final_body = "Feeling sad";
 
-    uniform_int_distribution<int> height_dist(150, 200);
-    uniform_int_distribution<int> weight_dist(100, 240);  // [50, 120]
-    uniform_int_distribution<int> building_dist(1, 300);
-    uniform_int_distribution<int> word_dist(0, WORDS.size() - 1);
+    Database db;
+    db.Put({"id", "Have a hand", "not-master", 1536107260, 10});
+    db.Erase("id");
+    db.Put({"id", final_body, "not-master", 1536107260, -10});
 
-    PersonHasher hasher;
-
-    const size_t num_buckets = 2053;
-    const size_t perfect_bucket_size = 50;
-    const size_t num_points = num_buckets * perfect_bucket_size;
-    vector<size_t> buckets(num_buckets);
-    for (size_t t = 0; t < num_points; ++t) {
-        Person person;
-        person.name = WORDS[word_dist(gen)];
-        person.height = height_dist(gen);
-        person.weight = weight_dist(gen) * 0.5;
-        person.address.city = WORDS[word_dist(gen)];
-        person.address.street = WORDS[word_dist(gen)];
-        person.address.building = building_dist(gen);
-        ++buckets[hasher(person) % num_buckets];
-    }
-    double pearson_stat = 0;
-    for (auto bucket_size : buckets) {
-        size_t size_diff = bucket_size - perfect_bucket_size;
-        pearson_stat +=
-                size_diff * size_diff / static_cast<double>(perfect_bucket_size);
-    }
-
-    const double critical_value = 2158.4981036918693;
-    ASSERT(pearson_stat < critical_value);
+    auto record = db.GetById("id");
+    ASSERT(record != nullptr);
+    ASSERT_EQUAL(final_body, record->title);
 }
 
 int main() {
     TestRunner tr;
-    RUN_TEST(tr, TestSmoke);
-    RUN_TEST(tr, TestPurity);
-    RUN_TEST(tr, TestDistribution);
-
+    RUN_TEST(tr, TestRangeBoundaries);
+    RUN_TEST(tr, TestSameUser);
+    RUN_TEST(tr, TestReplacement);
     return 0;
 }
-
