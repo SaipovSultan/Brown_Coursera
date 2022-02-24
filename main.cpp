a7ee052b3c2e6ddeb8563096192ff346d2868509
SultanMad <saipov.sultan46274@mail.ru>
2022-02-24 13:32:31 +0400

    Задача на программирование: Интеграция структуры Person в unordered_set.

--- main.cpp
+++ main.cpp
@@ -5,37 +5,75 @@
 
 using namespace std;
 
-using CoordType = int;
+struct Address {
+    string city, street;
+    int building;
 
-struct Point3D {
-    CoordType x;
-    CoordType y;
-    CoordType z;
-
-    bool operator==(const Point3D& other) const {
-        return (x == other.x) && (y == other.y) && (z == other.z);
+    bool operator==(const Address& other) const {
+        return city == other.city && street == other.street && building == other.building;
     }
 };
 
-struct Hasher {
-    size_t operator()(const Point3D& point3d) const{
-        const size_t prime = 3301;
-        hash<CoordType> coord_hasher;
-        return (coord_hasher(point3d.x)*prime*prime) +
-               (coord_hasher(point3d.y)*prime) +
-               coord_hasher(point3d.z);
+struct Person {
+    string name;
+    int height;
+    double weight;
+    Address address;
+
+    bool operator==(const Person& other) const {
+        return name == other.name && height == other.height
+               && weight == other.weight && address == other.address;
     }
+};
+
+struct AddressHasher {
+    size_t operator() (const Address& address) const {
+        const size_t prime = 3301;
+        hash<string> str_hash;
+        hash<int> int_hash;
+        return  (str_hash(address.city)*prime*prime) +
+                (str_hash(address.street)*prime) + int_hash(address.building);
+    }
+};
+
+struct PersonHasher {
+    size_t operator() (const Person& person) const {
+        const size_t prime = 3301;
+        hash<string> str_hash;
+        hash<int> int_hash;
+        hash<double> double_hash;
+        AddressHasher address_hasher;
+        return  (str_hash(person.name)*prime*prime*prime) +
+                (int_hash(person.height)*prime*prime) + (double_hash(person.weight)*prime) +
+                address_hasher(person.address);
+    }
+};
+
+ const vector<string> WORDS = {
+        "Kieran", "Jong", "Jisheng", "Vickie", "Adam", "Simon", "Lance",
+        "Everett", "Bryan", "Timothy", "Daren", "Emmett", "Edwin", "List",
+        "Sharon", "Trying", "Dan", "Saad", "Kamiya", "Nikolai", "Del",
+        "Casper", "Arthur", "Mac", "Rajesh", "Belinda", "Robin", "Lenora",
+        "Carisa", "Penny", "Sabrina", "Ofer", "Suzanne", "Pria", "Magnus",
+        "Ralph", "Cathrin", "Phill", "Alex", "Reinhard", "Marsh", "Tandy",
+        "Mongo", "Matthieu", "Sundaresan", "Piotr", "Ramneek", "Lynne", "Erwin",
+        "Edgar", "Srikanth", "Kimberly", "Jingbai", "Lui", "Jussi", "Wilmer",
+        "Stuart", "Grant", "Hotta", "Stan", "Samir", "Ramadoss", "Narendra",
+        "Gill", "Jeff", "Raul", "Ken", "Rahul", "Max", "Agatha",
+        "Elizabeth", "Tai", "Ellen", "Matt", "Ian", "Toerless", "Naomi",
+        "Rodent", "Terrance", "Ethan", "Florian", "Rik", "Stanislaw", "Mott",
+        "Charlie", "Marguerite", "Hitoshi", "Panacea", "Dieter", "Randell", "Earle",
+        "Rajiv", "Ted", "Mann", "Bobbie", "Pat", "Olivier", "Harmon",
+        "Raman", "Justin"
 };
 
 void TestSmoke() {
-    vector<Point3D> points = {
-            {1, 2, 3},
-            {0, 2, 3},
-            {1, 0, 3},
-            {1, 2, 0},
+    vector<Person> points = {
+            {"John", 180, 82.5, {"London", "Baker St", 221}},
+            {"Sherlock", 190, 75.3, {"London", "Baker St", 221}},
     };
 
-    unordered_set<Point3D, Hasher> point_set;
+    unordered_set<Person, PersonHasher> point_set;
     for (const auto& point : points) {
         point_set.insert(point);
     }
@@ -47,104 +85,45 @@ void TestSmoke() {
 }
 
 void TestPurity() {
-    Point3D point = {1, 2, 3};
-    Hasher hasher;
+    Person person = {"John", 180, 82.5, {"London", "Baker St", 221}};
+    PersonHasher hasher;
 
-    auto hash = hasher(point);
+    auto hash = hasher(person);
     for (size_t t = 0; t < 100; ++t) {
-        ASSERT_EQUAL(hasher(point), hash);
+        ASSERT_EQUAL(hasher(person), hash);
     }
-};
-
-void TestX() {
-    auto seed = 42;
-    mt19937 gen(seed);
-    uniform_int_distribution<CoordType> dist(
-            numeric_limits<CoordType>::min(),
-            numeric_limits<CoordType>::max()
-    );
-
-    Hasher hasher;
-
-    bool component_matters = false;
-    for (size_t t = 0; t < 1000; ++t) {
-        Point3D point = {dist(gen), dist(gen), dist(gen)};
-        Point3D other_point = point;
-        other_point.x = dist(gen);
-
-        component_matters |= (hasher(point) != hasher(other_point));
-    }
-
-    ASSERT(component_matters);
-}
-
-void TestY() {
-    auto seed = 42;
-    mt19937 gen(seed);
-    uniform_int_distribution<CoordType> dist(
-            numeric_limits<CoordType>::min(),
-            numeric_limits<CoordType>::max()
-    );
-
-    Hasher hasher;
-
-    bool component_matters = false;
-    for (size_t t = 0; t < 1000; ++t) {
-        Point3D point = {dist(gen), dist(gen), dist(gen)};
-        Point3D other_point = point;
-        other_point.y = dist(gen);
-
-        component_matters |= (hasher(point) != hasher(other_point));
-    }
-
-    ASSERT(component_matters);
-}
-
-void TestZ() {
-    auto seed = 42;
-    mt19937 gen(seed);
-    uniform_int_distribution<CoordType> dist(
-            numeric_limits<CoordType>::min(),
-            numeric_limits<CoordType>::max()
-    );
-
-    Hasher hasher;
-
-    bool component_matters = false;
-    for (size_t t = 0; t < 1000; ++t) {
-        Point3D point = {dist(gen), dist(gen), dist(gen)};
-        Point3D other_point = point;
-        other_point.z = dist(gen);
-
-        component_matters |= (hasher(point) != hasher(other_point));
-    }
-
-    ASSERT(component_matters);
 }
 
 void TestDistribution() {
     auto seed = 42;
     mt19937 gen(seed);
-    uniform_int_distribution<CoordType> dist(
-            numeric_limits<CoordType>::min(),
-            numeric_limits<CoordType>::max()
-    );
 
-    Hasher hasher;
+    uniform_int_distribution<int> height_dist(150, 200);
+    uniform_int_distribution<int> weight_dist(100, 240);  // [50, 120]
+    uniform_int_distribution<int> building_dist(1, 300);
+    uniform_int_distribution<int> word_dist(0, WORDS.size() - 1);
+
+    PersonHasher hasher;
 
     const size_t num_buckets = 2053;
     const size_t perfect_bucket_size = 50;
     const size_t num_points = num_buckets * perfect_bucket_size;
     vector<size_t> buckets(num_buckets);
     for (size_t t = 0; t < num_points; ++t) {
-        Point3D point = {dist(gen), dist(gen), dist(gen)};
-        ++buckets[hasher(point) % num_buckets];
+        Person person;
+        person.name = WORDS[word_dist(gen)];
+        person.height = height_dist(gen);
+        person.weight = weight_dist(gen) * 0.5;
+        person.address.city = WORDS[word_dist(gen)];
+        person.address.street = WORDS[word_dist(gen)];
+        person.address.building = building_dist(gen);
+        ++buckets[hasher(person) % num_buckets];
     }
     double pearson_stat = 0;
-    for (auto bucket_count : buckets) {
-        size_t count_diff = bucket_count - perfect_bucket_size;
+    for (auto bucket_size : buckets) {
+        size_t size_diff = bucket_size - perfect_bucket_size;
         pearson_stat +=
-                count_diff * count_diff / static_cast<double>(perfect_bucket_size);
+                size_diff * size_diff / static_cast<double>(perfect_bucket_size);
     }
 
     const double critical_value = 2158.4981036918693;
@@ -155,10 +134,8 @@ int main() {
     TestRunner tr;
     RUN_TEST(tr, TestSmoke);
     RUN_TEST(tr, TestPurity);
-    RUN_TEST(tr, TestX);
-    RUN_TEST(tr, TestY);
-    RUN_TEST(tr, TestZ);
     RUN_TEST(tr, TestDistribution);
 
     return 0;
 }
+
