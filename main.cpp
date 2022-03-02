9bf80c6e3a23f5069bd39748d7a7fd901e6929f9
Sultan Saipov <saipov-1995@mail.ru>
2022-03-02 12:15:25 +0300

    Тренировочное задание по программированию: Реструктуризация кода.

--- main.cpp
+++ main.cpp
@@ -1,108 +1,60 @@
-#include "xml.h"
-#include "json.h"
-
 #include "test_runner.h"
+#include "stats_aggregator.h"
 
 #include <vector>
 #include <string>
 #include <map>
-
+#include <memory>
+#include <iostream>
+#include <unordered_map>
+#include <functional>
 using namespace std;
 
-Json::Document XmlToJson(const Xml::Document& doc) {
-    vector<Json::Node> result;
-    for (const Xml::Node& n : doc.GetRoot().Children()) {
-        result.emplace_back(map<string, Json::Node>{
-                {"category", Json::Node{n.AttributeValue<string>("category")}},
-                {"amount", Json::Node(n.AttributeValue<int>("amount"))}
-        });
-    }
-    return Json::Document(Json::Node{std::move(result)});
-}
+void TestAll();
 
-Xml::Document JsonToXml(const Json::Document& doc, string root_name) {
-    Xml::Node root(move(root_name), {});
-    for (const Json::Node& n : doc.GetRoot().AsArray()) {
-        root.AddChild(Xml::Node("spend", {
-                {"category", n.AsMap().at("category").AsString()},
-                {"amount", to_string(n.AsMap().at("amount").AsInt())},
-        }));
-    }
-    return Xml::Document(root);
-}
-
-void TestXmlToJson() {
-    Xml::Node root("july", {});
-    root.AddChild({"spend", {{"category", "travel"}, {"amount", "23400"}}});
-    root.AddChild({"spend", {{"category", "food"}, {"amount", "5000"}}});
-    root.AddChild({"spend", {{"category", "transport"}, {"amount", "1150"}}});
-    root.AddChild({"spend", {{"category", "sport"}, {"amount", "12000"}}});
-    const Xml::Document xml_doc(std::move(root));
-
-    const auto json_doc = XmlToJson(xml_doc);
-
-    const auto& items = json_doc.GetRoot().AsArray();
-    ASSERT_EQUAL(items.size(), 4u);
-
-    const vector<string> expected_category = {"travel", "food", "transport", "sport"};
-    const vector<int> expected_amount = {23400, 5000, 1150, 12000};
-
-    for (size_t i = 0; i < items.size(); ++i) {
-        const map<string, Json::Node>& item = items[i].AsMap();
-        const string feedback_msg = "i = " + std::to_string(i);
-        AssertEqual(item.at("category").AsString(), expected_category[i], feedback_msg);
-        AssertEqual(item.at("amount").AsInt(), expected_amount[i], feedback_msg);
-    }
-}
-
-void TestJsonToXml() {
-
-    const Json::Document json_doc{Json::Node(vector<Json::Node>{
-            Json::Node(map<string, Json::Node>{
-                    {"category", Json::Node("food")}, {"amount", Json::Node(2500)}
-            }),
-            Json::Node(map<string, Json::Node>{
-                    {"category", Json::Node("transport")}, {"amount", Json::Node(1150)}
-            }),
-            Json::Node(map<string, Json::Node>{
-                    {"category", Json::Node("restaurants")}, {"amount", Json::Node(5780)}
-            }),
-            Json::Node(map<string, Json::Node>{
-                    {"category", Json::Node("clothes")}, {"amount", Json::Node(7500)}
-            }),
-            Json::Node(map<string, Json::Node>{
-                    {"category", Json::Node("travel")}, {"amount", Json::Node(23740)}
-            }),
-            Json::Node(map<string, Json::Node>{
-                    {"category", Json::Node("sport")}, {"amount", Json::Node(12000)}
-            }),
-    })};
-
-    const string root_name = "month";
-    const auto xml_doc = JsonToXml(json_doc, root_name);
-    const Xml::Node& root = xml_doc.GetRoot();
-
-    ASSERT_EQUAL(root.Name(), root_name);
-    const vector<Xml::Node>& children = root.Children();
-    ASSERT_EQUAL(children.size(), 6u);
-
-    const vector<string> expected_category = {
-            "food", "transport", "restaurants", "clothes", "travel", "sport"
+unique_ptr<StatsAggregator> ReadAggregators(istream& input) {
+    using namespace StatsAggregators;
+    const unordered_map<string, std::function<unique_ptr<StatsAggregator>()>> known_builders = {
+            {"sum", [] { return make_unique<Sum>(); }},
+            {"min", [] { return make_unique<Min>(); }},
+            {"max", [] { return make_unique<Max>(); }},
+            {"avg", [] { return make_unique<Average>(); }},
+            {"mode", [] { return make_unique<Mode>(); }}
     };
-    const vector<int> expected_amount = {2500, 1150, 5780, 7500, 23740, 12000};
 
-    for (size_t i = 0; i < children.size(); ++i) {
-        const string feedback_msg = "i = " + std::to_string(i);
-        const Xml::Node& c = children[i];
-        AssertEqual(c.Name(), "spend", feedback_msg);
-        AssertEqual(c.AttributeValue<string>("category"), expected_category[i], feedback_msg);
-        AssertEqual(c.AttributeValue<int>("amount"), expected_amount[i], feedback_msg);
+    auto result = make_unique<Composite>();
+
+    int aggr_count;
+    input >> aggr_count;
+
+    string line;
+    for (int i = 0; i < aggr_count; ++i) {
+        input >> line;
+        result->Add(known_builders.at(line)());
     }
+
+    return result;
 }
 
 int main() {
-    TestRunner tr;
-    RUN_TEST(tr, TestXmlToJson);
-    RUN_TEST(tr, TestJsonToXml);
+    TestAll();
+
+    auto stats_aggregator = ReadAggregators(cin);
+
+    for (int value; cin >> value; ) {
+        stats_aggregator->Process(value);
+    }
+    stats_aggregator->PrintValue(cout);
+
     return 0;
+}
+
+void TestAll() {
+    TestRunner tr;
+    RUN_TEST(tr, StatsAggregators::TestSum);
+    RUN_TEST(tr, StatsAggregators::TestMin);
+    RUN_TEST(tr, StatsAggregators::TestMax);
+    RUN_TEST(tr, StatsAggregators::TestAverage);
+    RUN_TEST(tr, StatsAggregators::TestMode);
+    RUN_TEST(tr, StatsAggregators::TestComposite);
 }
