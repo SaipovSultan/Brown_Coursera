19c018358c89ce66ffd4c09afd5910e93adb6194
Sultan Saipov <saipov-1995@mail.ru>
2022-03-06 16:16:36 +0300

    Задание по программированию: Константность в шаблоне ConcurrentMap часть 1.

--- main.cpp
+++ main.cpp
@@ -1,84 +1,259 @@
 #include "test_runner.h"
+#include "profile.h"
 #include "synchronized.h"
-#include <numeric>
-#include <vector>
-#include <string>
+
 #include <future>
 #include <mutex>
-#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <random>
 using namespace std;
 
-void TestConcurrentUpdate() {
-    Synchronized<string> common_string;
+template <typename K, typename V, typename Hash = std::hash<K>>
+class ConcurrentMap {
+public:
+    using MapType = unordered_map<K, V, Hash>;
 
-    const size_t add_count = 50000;
-    auto updater = [&common_string, add_count] {
-        for (size_t i = 0; i < add_count; ++i) {
-            auto access = common_string.GetAccess();
-            access.ref_to_value += 'a';
+    struct WriteAccess {
+        std::lock_guard<std::mutex> lock;
+        V& ref_to_value;
+    };
+
+    struct ReadAccess {
+        std::lock_guard<std::mutex> lock;
+        const V& ref_to_value;
+    };
+
+    explicit ConcurrentMap(size_t bucket_count) : bucket_count(bucket_count), buckets(bucket_count), mtxs(bucket_count){}
+
+    WriteAccess operator[](const K& key){
+        size_t index_bucket = hasher(key) % bucket_count;
+        MapType& bucket = buckets[index_bucket];
+        return {std::lock_guard<std::mutex>(mtxs[index_bucket]), bucket[key]};
+    }
+
+    ReadAccess At(const K& key) const{
+        size_t index_bucket = hasher(key) % bucket_count;
+        const MapType& bucket = buckets[index_bucket];
+        return {std::lock_guard<std::mutex>(mtxs[index_bucket]), bucket.at(key)};
+    }
+
+    bool Has(const K& key) const{
+        size_t index_bucket = hasher(key) % bucket_count;
+        std::lock_guard<mutex> lock(mtxs[index_bucket]);
+        const MapType& bucket = buckets[index_bucket];
+        return bucket.count(key) != 0;
+    }
+
+    MapType BuildOrdinaryMap() const{
+        MapType dict;
+        for(size_t index_bucket = 0;index_bucket < bucket_count;++index_bucket){
+            std::lock_guard<mutex> lock(mtxs[index_bucket]);
+            for(const auto& [key, value] : buckets[index_bucket]){
+                dict[key] = value;
+            }
+        }
+        return dict;
+    }
+
+private:
+    size_t bucket_count;
+    std::vector<MapType> buckets;
+    mutable std::vector<std::mutex> mtxs;
+    Hash hasher;
+};
+
+void RunConcurrentUpdates(
+        ConcurrentMap<int, int>& cm, size_t thread_count, int key_count
+) {
+    auto kernel = [&cm, key_count](int seed) {
+        vector<int> updates(key_count);
+        iota(begin(updates), end(updates), -key_count / 2);
+        shuffle(begin(updates), end(updates), default_random_engine(seed));
+
+        for (int i = 0; i < 2; ++i) {
+            for (auto key : updates) {
+                cm[key].ref_to_value++;
+            }
         }
     };
 
-    auto f1 = async(updater);
-    auto f2 = async(updater);
-
-    f1.get();
-    f2.get();
-
-    ASSERT_EQUAL(common_string.GetAccess().ref_to_value.size(), 2 * add_count);
+    vector<future<void>> futures;
+    for (size_t i = 0; i < thread_count; ++i) {
+        futures.push_back(async(kernel, i));
+    }
 }
 
-vector<int> Consume(Synchronized<deque<int>>& common_queue) {
-    vector<int> got;
+void TestConcurrentUpdate() {
+    const size_t thread_count = 3;
+    const size_t key_count = 50000;
 
-    for (;;) {
-        deque<int> q;
+    ConcurrentMap<int, int> cm(thread_count);
+    RunConcurrentUpdates(cm, thread_count, key_count);
 
-        {
-            auto access = common_queue.GetAccess();
-            q = move(access.ref_to_value);
+    const auto result = std::as_const(cm).BuildOrdinaryMap();
+    ASSERT_EQUAL(result.size(), key_count);
+    for (auto& [k, v] : result) {
+        AssertEqual(v, 6, "Key = " + to_string(k));
+    }
+}
+
+void TestReadAndWrite() {
+    ConcurrentMap<size_t, string> cm(5);
+
+    auto updater = [&cm] {
+        for (size_t i = 0; i < 50000; ++i) {
+            cm[i].ref_to_value += 'a';
         }
+    };
+    auto reader = [&cm] {
+        vector<string> result(50000);
+        for (size_t i = 0; i < result.size(); ++i) {
+            result[i] = cm[i].ref_to_value;
+        }
+        return result;
+    };
 
-        for (int item : q) {
-            if (item > 0) {
-                got.push_back(item);
-            } else {
-                return got;
+    auto u1 = async(updater);
+    auto r1 = async(reader);
+    auto u2 = async(updater);
+    auto r2 = async(reader);
+
+    u1.get();
+    u2.get();
+
+    for (auto f : {&r1, &r2}) {
+        auto result = f->get();
+        ASSERT(all_of(result.begin(), result.end(), [](const string& s) {
+            return s.empty() || s == "a" || s == "aa";
+        }));
+    }
+}
+
+void TestSpeedup() {
+    {
+        ConcurrentMap<int, int> single_lock(1);
+
+        LOG_DURATION("Single lock");
+        RunConcurrentUpdates(single_lock, 4, 50000);
+    }
+    {
+        ConcurrentMap<int, int> many_locks(100);
+
+        LOG_DURATION("100 locks");
+        RunConcurrentUpdates(many_locks, 4, 50000);
+    }
+}
+
+void TestConstAccess() {
+    const unordered_map<int, string> expected = {
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {31, "thirty one"},
+            {127, "one hundred and twenty seven"},
+            {1598, "fifteen hundred and ninety eight"}
+    };
+
+    const ConcurrentMap<int, string> cm = [&expected] {
+        ConcurrentMap<int, string> result(3);
+        for (const auto& [k, v] : expected) {
+            result[k].ref_to_value = v;
+        }
+        return result;
+    }();
+
+    vector<future<string>> futures;
+    for (int i = 0; i < 10; ++i) {
+        futures.push_back(async([&cm, i] {
+            try {
+                return cm.At(i).ref_to_value;
+            } catch (exception&) {
+                return string();
             }
+        }));
+    }
+    futures.clear();
+
+    ASSERT_EQUAL(cm.BuildOrdinaryMap(), expected);
+}
+
+void TestStringKeys() {
+    const unordered_map<string, string> expected = {
+            {"one", "ONE"},
+            {"two", "TWO"},
+            {"three", "THREE"},
+            {"thirty one", "THIRTY ONE"},
+    };
+
+    const ConcurrentMap<string, string> cm = [&expected] {
+        ConcurrentMap<string, string> result(2);
+        for (const auto& [k, v] : expected) {
+            result[k].ref_to_value = v;
         }
+        return result;
+    }();
+
+    ASSERT_EQUAL(cm.BuildOrdinaryMap(), expected);
+}
+
+struct Point {
+    int x, y;
+};
+
+struct PointHash {
+    size_t operator()(Point p) const {
+        std::hash<int> h;
+        return h(p.x) * 3571 + h(p.y);
+    }
+};
+
+bool operator==(Point lhs, Point rhs) {
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+void TestUserType() {
+    ConcurrentMap<Point, size_t, PointHash> point_weight(5);
+
+    vector<future<void>> futures;
+    for (int i = 0; i < 1000; ++i) {
+        futures.push_back(async([&point_weight, i] {
+            point_weight[Point{i, i}].ref_to_value = i;
+        }));
+    }
+
+    futures.clear();
+
+    for (int i = 0; i < 1000; ++i) {
+        ASSERT_EQUAL(point_weight.At(Point{i, i}).ref_to_value, i);
+    }
+
+    const auto weights = point_weight.BuildOrdinaryMap();
+    for (int i = 0; i < 1000; ++i) {
+        ASSERT_EQUAL(weights.at(Point{i, i}), i);
     }
 }
 
-void Log(const Synchronized<deque<int>>& common_queue, ostream& out) {
-    for (int i = 0; i < 100; ++i) {
-        out << "Queue size is " << common_queue.GetAccess().ref_to_value.size() << '\n';
-    }
-}
+void TestHas() {
+    ConcurrentMap<int, int> cm(2);
+    cm[1].ref_to_value = 100;
+    cm[2].ref_to_value = 200;
 
-void TestProducerConsumer() {
-    Synchronized<deque<int>> common_queue;
-    ostringstream log;
-
-    auto consumer = async(Consume, ref(common_queue));
-    auto logger = async(Log, cref(common_queue), ref(log));
-
-    const size_t item_count = 100000;
-    for (size_t i = 1; i <= item_count; ++i) {
-        common_queue.GetAccess().ref_to_value.push_back(i);
-    }
-    common_queue.GetAccess().ref_to_value.push_back(-1);
-
-    vector<int> expected(item_count);
-    iota(begin(expected), end(expected), 1);
-    ASSERT_EQUAL(consumer.get(), expected);
-
-    logger.get();
-    const string logs = log.str();
-    ASSERT(!logs.empty());
+    const auto& const_map = std::as_const(cm);
+    ASSERT(const_map.Has(1));
+    ASSERT(const_map.Has(2));
+    ASSERT(!const_map.Has(3));
 }
 
 int main() {
     TestRunner tr;
     RUN_TEST(tr, TestConcurrentUpdate);
-    RUN_TEST(tr, TestProducerConsumer);
+    RUN_TEST(tr, TestReadAndWrite);
+    RUN_TEST(tr, TestSpeedup);
+    RUN_TEST(tr, TestConstAccess);
+    RUN_TEST(tr, TestStringKeys);
+    RUN_TEST(tr, TestUserType);
+    RUN_TEST(tr, TestHas);
 }
