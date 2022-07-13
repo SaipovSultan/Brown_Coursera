13fec4dfa52519924f407b76a374e81486f2c24d
SaipovSultan <saipov-1995@mail.ru>
2022-07-14 00:06:04 +0300

    Задание по программированию: Система бронирования отелей.

--- main.cpp
+++ main.cpp
@@ -1,73 +1,100 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <map>
+#include <string>
+#include <queue>
+
 
 using namespace std;
 
-enum class Gender {
-    FEMALE,
-    MALE
-};
-
-struct Person {
-    int age;  // возраст
-    Gender gender;  // пол
-    bool is_employed;  // имеет ли работу
-};
-
-
-template <typename InputIt>
-int ComputeMedianAge(InputIt range_begin, InputIt range_end) {
-    if (range_begin == range_end) {
-        return 0;
+class HotelManager {
+public:
+    void Book(string hotel_name, int64_t time, int client_id, int room_count) {
+        current_time = time;
+        hotels[hotel_name].Book({time, client_id, room_count});
     }
-    vector<typename InputIt::value_type> range_copy(range_begin, range_end);
-    auto middle = begin(range_copy) + range_copy.size() / 2;
-    nth_element(
-            begin(range_copy), middle, end(range_copy),
-            [](const Person& lhs, const Person& rhs) {
-                return lhs.age < rhs.age;
+
+    int ComputeClientsCount(string hotel_name) {
+        return hotels[hotel_name].ComputeClientsCount(current_time);
+    }
+
+    int ComputeRoomsCount(string hotel_name) {
+        return hotels[hotel_name].ComputeRoomsCount(current_time);
+    }
+
+private:
+    struct Booking {
+        int64_t time;
+        int client_id;
+        int room_count;
+    };
+
+    class HotelInfo {
+    public:
+        void Book(const Booking& booking) {
+            last_bookings.push(booking);
+            rooms_count += booking.room_count;
+            client_booking_count[booking.client_id] += 1;
+        }
+
+        int ComputeClientsCount(int64_t current_time) {
+            RemoveOldBookings(current_time);
+            return client_booking_count.size();
+        }
+
+        int ComputeRoomsCount(int64_t current_time) {
+            RemoveOldBookings(current_time);
+            return rooms_count;
+        }
+    private:
+        queue<Booking> last_bookings;
+        map<int, int> client_booking_count;
+        int rooms_count = 0;
+        static const int TIME_WINDOW_SIZE = 86400;
+
+        void RemoveOldBookings(int64_t current_time) {
+            while(!last_bookings.empty() && current_time - last_bookings.front().time >= TIME_WINDOW_SIZE) {
+                PopBooking();
             }
-    );
-    return middle->age;
-}
+        }
 
-void PrintStats(std::vector<Person> persons) {
-    const int median_age = ComputeMedianAge(std::begin(persons), std::end(persons));
+        void PopBooking() {
+            const Booking& booking = last_bookings.front();
+            rooms_count -= booking.room_count;
+            const auto cliet_stat_it = client_booking_count.find(booking.client_id);
+            if(--cliet_stat_it->second == 0) {
+                client_booking_count.erase(booking.client_id);
+            }
+            last_bookings.pop();
+        }
+    };
 
-    auto female_begin = std::partition(std::begin(persons), std::end(persons), [](const Person& person) { return person.gender == Gender::MALE;});
-    const int median_age_female = ComputeMedianAge(female_begin, std::end(persons));
-    const int median_age_male = ComputeMedianAge(std::begin(persons), female_begin);
-
-    auto female_unemployed_begin = std::partition(female_begin, std::end(persons), [](const Person& person) { return person.is_employed;});
-    const int median_age_female_employed = ComputeMedianAge(female_begin, female_unemployed_begin);
-    const int median_age_female_unemployed = ComputeMedianAge(female_unemployed_begin, std::end(persons));
-
-    auto male_unemployed_begin = std::partition(std::begin(persons), female_begin, [](const Person& person) { return person.is_employed;});
-    const int median_age_male_employed = ComputeMedianAge(std::begin(persons), male_unemployed_begin);
-    const int median_age_male_unemployed = ComputeMedianAge(male_unemployed_begin, female_begin);
-
-    cout << "Median age = " <<  median_age << "\n";
-    cout << "Median age for females = " <<  median_age_female << "\n";
-    cout << "Median age for males = " <<  median_age_male << "\n";
-    cout << "Median age for employed females = " <<  median_age_female_employed << "\n";
-    cout << "Median age for unemployed females = " <<  median_age_female_unemployed << "\n";
-    cout << "Median age for employed males = " <<  median_age_male_employed << "\n";
-    cout << "Median age for unemployed males = " <<  median_age_male_unemployed << "\n";
-
-}
+    int64_t current_time;
+    map<string, HotelInfo> hotels;
+};
 
 int main() {
-    vector<Person> persons = {
-            {31, Gender::MALE, false},
-            {40, Gender::FEMALE, true},
-            {24, Gender::MALE, true},
-            {20, Gender::FEMALE, true},
-            {80, Gender::FEMALE, false},
-            {78, Gender::MALE, false},
-            {10, Gender::FEMALE, false},
-            {55, Gender::MALE, true},
-    };
-    PrintStats(persons);
+    int count_query;
+    cin >> count_query;
+    string type_query;
+    HotelManager hotelManager;
+    for(int i = 0;i < count_query;++i) {
+        cin >> type_query;
+        if(type_query == "BOOK") {
+            int64_t time, client_id;
+            short room_count;
+            string hotel_name;
+            cin >> time >> hotel_name >> client_id >> room_count;
+            hotelManager.Book(hotel_name, time, client_id, room_count);
+        } else if(type_query == "CLIENTS") {
+            string hotel_name;
+            cin >> hotel_name;
+            cout << hotelManager.ComputeClientsCount(hotel_name) << "\n";
+        } else if(type_query == "ROOMS") {
+            string hotel_name;
+            cin >> hotel_name;
+            cout << hotelManager.ComputeRoomsCount(hotel_name) << "\n";
+        }
+    }
+    cout.flush();
     return 0;
 }
